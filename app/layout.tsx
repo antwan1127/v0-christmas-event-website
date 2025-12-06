41a53107956b3e7a9c66caacb9d5c37258f8f1a2
Sam <samuellsameh611@gmail.com>
2025-12-06 12:53:19 +0200

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="preload" href="/fonts/TIDO-B.otf" as="font" type="font/otf" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/Balbaleo.otf" as="font" type="font/otf" crossOrigin="anonymous" />
       </head>
